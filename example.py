0b4d0d2fea76f4336b3042b54901f78f8ef2cec8
nichind <nichinddev@gmail.com>
2024-08-18 15:28:28 +0300

    crystalpay tested

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ async def main():
     cryptobot = Provider('cryptobot', api_key='', network='test')
     pay = EasyPay(network='test', providers=[crystalpay, cryptobot])
 
-    invoice = await pay.create_invoice(0.25, 'TON', 'cryptobot')
+    invoice = await pay.create_invoice(15, 'RUB', 'crystalpay')
     while invoice.status != 'paid':
         await sleep(5)
         await invoice.check()
--- pyeasypay/core/providers/crystalpay.py
+++ pyeasypay/core/providers/crystalpay.py
@@ -1,7 +1,4 @@
-from AaioAPI import AsyncAaioAPI
-from hashlib import sha256
-from uuid import uuid4
-from asyncio import wait_for
+from requests import post
 
 
 class Invoice:
@@ -16,8 +13,43 @@ class Invoice:
         if 'secret' not in self.creds.__dict__.keys():
             raise ValueError(f'secret is required for {self.creds.name} provider')
 
+        if self.invoice.currency != 'RUB':
+            raise ValueError(f'Only RUB currency is supported for {self.creds.name} provider')
+
     async def create(self):
-        pass
+        url = "https://api.crystalpay.io/v2/invoice/create/"
+        payload = {
+            "auth_login": self.creds.login,
+            "auth_secret": self.creds.secret,
+            "amount": self.amount,
+            "type": 'purchase',
+            "description": 'CrystalPay payment',
+            "redirect_url": self.creds.redirect_url if 'redirect_url' in self.creds.__dict__.keys() else "https://nichind.dev",
+            "callback_url": self.creds.callback_url if 'callback_url' in self.creds.__dict__.keys() else "https://nichind.dev",
+            "lifetime": 1440
+        }
+        headers = {'Content-Type': 'application/json'}
+
+        response = post(url, json=payload, headers=headers)
+        data = response.json()
+
+        if data.get("error"):
+            raise ValueError(data.get("errors"))
+        self.invoice.identifier = data.get("id")
+        self.invoice.pay_info = data.get("url")
+        return self.invoice.pay_info
 
     async def check(self):
-        pass
+        url = "https://api.crystalpay.io/v2/invoice/info/"
+        payload = {
+            "auth_login": self.creds.login,
+            "auth_secret": self.creds.secret,
+            "id": self.invoice.identifier
+        }
+        headers = {'Content-Type': 'application/json'}
+
+        response = post(url, json=payload, headers=headers)
+        data = response.json()
+
+        self.invoice.status = 'paid' if data.get("state") == 'payed' else 'pending'
+        return self.invoice.status
