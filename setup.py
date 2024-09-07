a013d5abf36479d83ea574086e8363203487fcd0
nichind <nichinddev@gmail.com>
2024-09-07 11:34:05 +0300

    bump ver for tests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='pyeasypay',
-    version='0.2.6',
+    version='0.2.7',
     author='nichind',
     author_email='nichinddev@gmail.com',
     description='Make money from your Python projects the easy way.',
